5585cfb34f131bb5b25401cb30e053687d98a8ca
Amir-ZoghiPeyman <zoghipeyman.amir@gmail.com>
2024-09-07 22:41:55 +0330

    footer added

--- mid-term-project/tailwind.config.js
+++ mid-term-project/tailwind.config.js
@@ -2,6 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    container: {
+      padding: {
+        DEFAULT: '15px',
+      }
+    },
+    screens: {
+      sm: '640px' ,
+      md: '768px' ,
+      lg: '960px' ,
+      xl: '1200px' ,
+    },
     extend: {},
   },
   plugins: [],
