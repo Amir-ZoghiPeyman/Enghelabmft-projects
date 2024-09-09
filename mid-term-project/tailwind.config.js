9442e75f4aba1c13e5f7247f3e931d007b74d660
Amir-ZoghiPeyman <zoghipeyman.amir@gmail.com>
2024-09-09 22:17:13 +0330

    new navbar added

--- mid-term-project/tailwind.config.js
+++ mid-term-project/tailwind.config.js
@@ -4,17 +4,16 @@ module.exports = {
   theme: {
     container: {
       padding: {
-        DEFAULT: '15px',
-      }
+        DEFAULT: "15px",
+      },
     },
     screens: {
-      sm: '640px' ,
-      md: '768px' ,
-      lg: '960px' ,
-      xl: '1200px' ,
+      sm: "640px",
+      md: "768px",
+      lg: "960px",
+      xl: "1200px",
     },
     extend: {},
   },
   plugins: [],
-}
-
+};
