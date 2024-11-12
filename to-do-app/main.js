cb3482df76fd91c9721a38b8b5f1d01ca59ad354
Amir-ZoghiPeyman <zoghipeyman.amir@gmail.com>
2024-11-12 22:17:17 +0330

    main js codes added

--- to-do-app/main.js
+++ to-do-app/main.js
@@ -2,3 +2,30 @@
 const input = document.getElementById("input");
 const btn = document.getElementById("add-btn");
 const root = document.getElementById("root");
+let editableItemId = null;
+
+TODOS = [
+  {
+    id: 0,
+    text: "running",
+    isDone: false,
+  },
+  {
+    id: 1,
+    text: "shopping",
+    isDone: false,
+  },
+];
+
+function addTodo() {
+  const inputVal = input.value;
+  const newId = Math.floor(Math.random() * 10000000);
+  const newTodo = {
+    id: newId,
+    text: inputVal,
+    isDone: false,
+  };
+  TODOS.push(newTodo);
+  input.value = "";
+  render();
+}
