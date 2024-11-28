ceb9fa6a46702d171668df202ae3c9d5e3a7edc7
Amir-ZoghiPeyman <zoghipeyman.amir@gmail.com>
2024-11-28 09:13:39 +0330

    limited products js codes added

--- end-of-session-projects/t-shirt store/src/assets/js/main.js
+++ end-of-session-projects/t-shirt store/src/assets/js/main.js
@@ -1,7 +1,38 @@
+const mainPageProductsContainer = document.getElementById("main-page-products");
 
-
-function getLimitedProducts(limitCount = 4) {
-  fetch(`https://fakestoreapi.com/products?limit=$(limitCount)`)
+async function getLimitedProducts(limitCount = 4) {
+  return await fetch(`https://fakestoreapi.com/products?limit=${limitCount}`)
     .then((res) => res.json())
-    .then((json) => console.log(json));
+    .then((json) => json);
+}
+
+async function renderMainPageProducts() {
+  const products = await getLimitedProducts(4);
+
+  const template = products
+    .map((product) => {
+      const { title, image, price } = product;
+
+      return `
+    <div class="mt-10 grid lg:grid-cols-4 gap-10">
+        <div>
+          <div class="flex justify-center items-center mb-4">
+            <img
+              src="${image}"
+              class="rounded-2xl shadow-2xl"
+              alt=""
+            />
+          </div>
+          <p class="flex justify-center items-center font-bold">
+            ${title}
+          </p>
+          <p class="flex justify-center items-center">${price}</p>
+        </div>
+    </div>
+    `;
+    })
+    .join("");
+
+  mainPageProductsContainer.innerHTML = template;
+  console.log(renderMainPageProducts);
 }
